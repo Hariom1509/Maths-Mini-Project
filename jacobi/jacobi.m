a373449974a88da2a14c2e5cc34e2a02c334554a
Hariom Vyas <u19ee014@eed.svnit.ac.in>
2021-04-15 15:26:31 +0530

    Create jacobi.m
    Not giving iota value for output

---
+++ jacobi/jacobi.m
@@ -0,0 +1,33 @@
+clc;
+close all;
+A= [ 1 -1 0 0; 1 -2-4i 4i 1; 0 4i -3-4i 1; 0 1 1 -2+0.25j ]
+b = [10; 0; 0; 0]
+x=[0; 0; 0; 0]
+ 
+n=size(x,1);
+normVal=Inf; 
+
+tol=1e-5; itr=0;
+
+while normVal>tol
+    xold=x;
+ 
+    for i=1:n
+        sigma=0;
+ 
+        for j=1:n
+ 
+            if j~=i
+                sigma=sigma+A(i,j)*x(j);
+            end
+ 
+        end
+ 
+        x(i)=(1/A(i,i))*(b(i)-sigma);
+    end
+ 
+    itr=itr+1;
+    normVal=abs(xold-x);
+end
+
+fprintf('Solution of the system is : \n%f\n%f\n%f\n%f in %d iterations',x,itr);
